6ed524d60a719dbf1c8350ba6e3252f5bbd394fd
frstshmn <theskaters75@gmail.com>
2019-11-05 22:50:43 +0200

    Create compress.php

---
+++ compress.php
@@ -0,0 +1,14 @@
+<?php 
+	$string = "Hello";
+	$characters = str_split($string);
+ 
+    $binary = [];
+    foreach ($characters as $character) {
+        $data = unpack('H*', $character);
+        $binary[] = base_convert($data[1], 16, 2);
+    }
+ 	
+ 	$binary = implode('', $binary);
+
+    echo $binary;
+?>
