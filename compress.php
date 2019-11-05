793d6160312a861b7592c955d1e1b3863e621e03
frstshmn <theskaters75@gmail.com>
2019-11-05 23:40:33 +0200

    Compressed binary to digits

--- compress.php
+++ compress.php
@@ -1,4 +1,13 @@
 <?php 
+
+	// function CheckBit($bit)
+	// {
+	// 	if($bit == 1){
+
+	// 	}
+	// }
+
+
 	$string = "Hello";
 	$characters = str_split($string);
  
@@ -9,6 +18,31 @@
     }
  	
  	$binary = implode('', $binary);
- 	
-    echo $binary;
+
+ 	$count = 1;
+ 	$digits = 0;
+
+ 	for($i = 0; $i <= strlen($binary); $i++)
+ 	{
+ 		if($binary[$i] != $binary[0])
+ 		{
+ 			if($binary[$i] == $binary[$i-1])
+	 		{
+	 			$count = $count + 1;
+	 		}
+	 		else if($binary[$i] != $binary[$i-1])
+	 		{
+	 			$digits = $digits.$count;
+	 			$count = 1;
+	 		}
+ 		}
+ 		else{
+ 			continue;
+ 		}
+ 		
+
+ 	}
+	echo $binary;
+	echo "<br>";
+   	echo $digits;
 ?>
